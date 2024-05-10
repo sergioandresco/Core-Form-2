1e27f9733f86ca3b7ab1b966fc335acfe110fe0f
Sergio Andrés Cobos Suarez <Sergio.Cobos@TUBOLETA.SP>
2024-05-10 11:37:52 -0500

    fix URL options in the header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -12,7 +12,7 @@ function Header(){
             <div className='header--c'>
 
                 <div className='c--c-logo'>
-                    <a href='https://www.tuboleta.com/images/Tuboleta_Pass/home.html' className='c-logo--link-img'>
+                    <a href='https://www.tuboleta.com/' className='c-logo--link-img'>
                         <img className='link-img--img' src='https://www.tuboleta.com/img/ic_logotuboleta.svg' />
                     </a>
                 </div>
@@ -22,15 +22,15 @@ function Header(){
                         <h2 className='link--opt'>Tuboleta Pass</h2>
                     </a>
 
-                    <a className='c-opt--link' href='https://www.tuboleta.com/images/Tuboleta_Pass/home.html'>
+                    <a className='c-opt--link' href='https://portal.tuboleta.com/vmove/?utm_source=vmovehome&utm_medium=vmovehometb&utm_campaign=bannervmovetb&utm_id=vmovebanner&_gl=1*1hq7ztg*_ga*MTUxOTQxNDg5LjE3MTE1NjQyNjc.*_ga_0TVTJ30NVQ*MTcxNTM1ODgwNi4xMTcuMS4xNzE1MzU4ODA2LjYwLjAuMTY2ODIwNDkwNQ..'>
                         <h2 className='link--opt'>Tuboleta Te Lleva</h2>
                     </a>
 
-                    <a className='c-opt--link' href='https://www.tuboleta.com/images/Tuboleta_Pass/home.html'>
+                    <a className='c-opt--link' href='https://web.tuboleta.com/images/SITIOS2020/Pasala/index.html?_gl=1*1hq7ztg*_ga*MTUxOTQxNDg5LjE3MTE1NjQyNjc.*_ga_0TVTJ30NVQ*MTcxNTM1ODgwNi4xMTcuMS4xNzE1MzU4ODA2LjYwLjAuMTY2ODIwNDkwNQ..'>
                         <h2 className='link--opt'>Pásala</h2>
                     </a>
 
-                    <a className='c-opt--link' href='https://www.tuboleta.com/images/Tuboleta_Pass/home.html'>
+                    <a className='c-opt--link' href='https://all.checkout.tuboleta.com/account/login?_gl=1*1cgxgcr*_ga*MTUxOTQxNDg5LjE3MTE1NjQyNjc.*_ga_0TVTJ30NVQ*MTcxNTM1ODgwNi4xMTcuMS4xNzE1MzU4OTIwLjUyLjAuMTY2ODIwNDkwNQ..'>
                         <h2 className='link--opt'>{t("form.login")}</h2>
                     </a>
 
