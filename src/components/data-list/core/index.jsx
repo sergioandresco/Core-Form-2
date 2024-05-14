cf79aa09c0c0e5c89f613411a2ac31ccfd86f026
Sergio Andrés Cobos Suarez <Sergio.Cobos@TUBOLETA.SP>
2024-05-14 15:53:47 -0500

    fix map in data-list core component

--- src/components/data-list/core/index.jsx
+++ src/components/data-list/core/index.jsx
@@ -13,48 +13,48 @@ function DataList() {
     if (isLoading) return <Loader />;
     if (error) return <div>Error: {error.message}</div>;
 
-    return (
-        <div className="container-list-component">
-
-            <div className="container-list-component--c-list">
-
-                <div className="c-list--heading">
-                    <span>Data List Users Core Medellín 2024</span>
-                </div>
-                
-                <table className='c-list--table'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Franchise</th>
-                            <th>Digits</th>
-                            <th>Type Document</th>
-                            <th>Number Document</th>
-                            <th>Url Document</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data?.map((item) => (
-                            <tr key={item.name}>
-                                <td>{item.name}</td>
-                                <td>{item.franchise}</td>
-                                <td>{item.digits}</td>
-                                <td>{item.typeDocument}</td>
-                                <td>{item.documentNumber}</td>
-                                <td>
-                                    <a href={item.storedFileName} target='_blank'>
-                                        <img className='table-iconDownload' src={DownloadIcon} />
-                                    </a>
-                                </td>
+    if (data && Array.isArray(data.data)) {
+        return (
+            <div className="container-list-component">
+                <div className="container-list-component--c-list">
+                    <div className="c-list--heading">
+                        <span>Data List Users Core Medellín 2024</span>
+                    </div>
+                    
+                    <table className='c-list--table'>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Franchise</th>
+                                <th>Digits</th>
+                                <th>Type Document</th>
+                                <th>Number Document</th>
+                                <th>Url Document</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-
+                        </thead>
+                        <tbody>
+                            {data.data.map((item) => (
+                                <tr key={item.name}>
+                                    <td>{item.name}</td>
+                                    <td>{item.franchise}</td>
+                                    <td>{item.digits}</td>
+                                    <td>{item.typeDocument}</td>
+                                    <td>{item.documentNumber}</td>
+                                    <td>
+                                        <a href={item.storedFileName} target='_blank'>
+                                            <img className='table-iconDownload' src={DownloadIcon} />
+                                        </a>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
-        </div>
-    );
+        );
+    } else {
+        return <div>No se encontraron datos.</div>;
+    }
 }
 
 export { DataList };
