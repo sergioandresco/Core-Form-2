9a806c42869cbb4ff0269769c6fa7c414e38817e
Sergio Andrés Cobos Suarez <Sergio.Cobos@TUBOLETA.SP>
2024-05-14 17:40:35 -0500

    fix index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Form } from './components/form/core'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { DataList } from './components/data-list/core';
 
 import './App.css'
@@ -10,7 +10,7 @@ function App() {
       <div className='container-principal-app'>
         <Routes>
           <Route path='/8682AD4A-109E-4195-9039-BF86A7AD752F/form-core' element={<Form />} />
-          <Route path='/' element={<DataList />} />
+          <Route path='/6152173f7d6b18c7d839f544813d20a09a85c6e1807585cb2f518bb3c47edeaa/data-list-core' element={<DataList />} />
         </Routes>
       </div>
     </Router>
