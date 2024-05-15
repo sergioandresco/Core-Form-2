cdc6db9b03c53241caa4f00ac1d1c2f8f81b6ae4
Sergio Andrés Cobos Suarez <Sergio.Cobos@TUBOLETA.SP>
2024-05-14 21:02:32 -0500

    console log test

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/8682AD4A-109E-4195-9039-BF86A7AD752F/form-core' element={<Form />} />
           <Route path='/6152173f7d6b18c7d839f544813d20a09a85c6e1807585cb2f518bb3c47edeaa/data-list-core' element={<DataList />} />
+          <Route path='/' element={<div><h2>NUEVO COMPONENTE</h2></div>} />
         </Routes>
       </div>
     </Router>
